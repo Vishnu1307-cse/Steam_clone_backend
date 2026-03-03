6d0c862a5b2c600c6a732f68efa9420ae3331da5
Vishnu <svvishnu33@gmail.com>
2026-03-03 09:50:01 +0530

    Allowed admin access

--- src/app.js
+++ src/app.js
@@ -19,14 +19,10 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true
+    origin: allowedOrigins,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
 
